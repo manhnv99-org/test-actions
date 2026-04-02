9b24e67ed62a572783634e5cd4b61c546d20250f
Brian Nguyen <brian.nguyen@sbidm.com>
2026-04-02 20:13:56 +0700

    new

--- src/main/java/com/abhishek/StartApplication.java
+++ src/main/java/com/abhishek/StartApplication.java
@@ -12,11 +12,14 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
+
+        boolean abc = true ? true : false;
+
         model.addAttribute("title", "I have successfuly built a sprint boot application using Maven");
         model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
         return "index";
     }
-    
+
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class, args);
     }
